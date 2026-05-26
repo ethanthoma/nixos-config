849e22b661ccf48895bd5989ebc35ea180c8a336
Ethan Thoma <ethanthoma@gmail.com>
2026-05-26 11:46:19 -0700

    feat: declarative syncthing config

--- modules/nixos/syncthing.nix
+++ modules/nixos/syncthing.nix
@@ -2,6 +2,12 @@
 {
   flake.nixosModules.syncthing =
     { ... }:
+    let
+      devices = {
+        desktop.id = "EFCXJQQ-3ZJWG5G-WH6WZAI-3RVOQYW-GYMCZNQ-7I77GFA-LDICFI3-OTJ3HQZ";
+        phone.id = "EB43N4A-JMHCQPD-HMRS5OR-S2NZSHD-6MZZBNR-O6TY6FX-ALIAYXB-NZIG7AS";
+      };
+    in
     {
       services.syncthing = {
         enable = true;
@@ -9,6 +15,14 @@
         dataDir = "/home/ethanthoma";
         configDir = "/home/ethanthoma/.config/syncthing";
         openDefaultPorts = true;
+
+        settings = {
+          inherit devices;
+          folders."keepass" = {
+            path = "/home/ethanthoma/keepass";
+            devices = builtins.attrNames devices;
+          };
+        };
       };
     };
 }
