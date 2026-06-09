fafa224d4ba046b8115fe02770c8e67dd4a0d0ae
Ethan Thoma <ethanthoma@gmail.com>
2026-06-09 15:07:19 -0700

    feat: store nix per-user state under XDG dirs

--- modules/nixos/common.nix
+++ modules/nixos/common.nix
@@ -12,6 +12,7 @@
         optimise.automatic = true;
         settings = {
           auto-optimise-store = true;
+          use-xdg-base-directories = true;
           experimental-features = [
             "nix-command"
             "flakes"
