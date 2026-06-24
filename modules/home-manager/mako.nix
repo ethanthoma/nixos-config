f6e096864272e314dde5da3781ba5a37a58852bc
Ethan Thoma <ethanthoma@gmail.com>
2026-06-24 12:43:40 -0700

    feat: style yubikey touch notification

--- modules/home-manager/mako.nix
+++ modules/home-manager/mako.nix
@@ -9,15 +9,29 @@ in
       services.mako = {
         enable = true;
         settings = {
+          anchor = "top-center";
+          margin = "10";
           border-radius = "10";
           border-size = "2";
-          background-color = palette.base;
+          background-color = palette.surface;
           text-color = palette.text;
-          border-color = palette.gold;
-          progress-color = "over ${palette.surface}";
+          border-color = palette.overlay;
+          progress-color = "over ${palette.gold}";
           default-timeout = "4000";
+          max-visible = "5";
+          group-by = "app-name";
+          on-button-left = "dismiss";
+          on-button-right = "dismiss-all";
+          on-button-middle = "invoke-default-action";
           "urgency=high" = {
             border-color = palette.love;
+            default-timeout = "0";
+          };
+          "app-name=yubikey-touch-detector" = {
+            format = "<b>󰈷  %s</b>";
+            font = "MonaspiceNe Nerd Font 11";
+            border-color = palette.love;
+            default-timeout = "0"; # persist until the key is tapped (detector closes it)
           };
         };
       };
