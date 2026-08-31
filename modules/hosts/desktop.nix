1001c3e679fd96d481f2571533bc5b8d2c4e6ae7
Ethan Thoma <ethanthoma@gmail.com>
2026-08-31 15:01:28 -0700

    feat: add crawl4ai container with a generated api token

--- modules/hosts/desktop.nix
+++ modules/hosts/desktop.nix
@@ -197,6 +197,7 @@ in
       self.nixosModules.moonlander
       self.nixosModules.steam
       self.nixosModules.podman
+      self.nixosModules.crawl4ai
       self.nixosModules.k3s
       self.nixosModules.yubikey
       self.nixosModules.yubikey-pam
